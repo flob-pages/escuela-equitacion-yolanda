aaea3be176307561001b8eb0acf8f7c4c343fd2f
Fabián Orozco <f.orozcob@uniandes.edu.co>
2025-08-05 10:07:49 -0500

    fix next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://www.equitacionsubachoque.com' : '',
   basePath: '',
   images: {
     unoptimized: true,
