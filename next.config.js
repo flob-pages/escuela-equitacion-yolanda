07816bded8050fd0b6ce5084d0fca767db7fd576
Fabián Orozco <f.orozcob@uniandes.edu.co>
2025-08-05 09:18:37 -0500

    Fix: Configuración para mostrar estilos en GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
