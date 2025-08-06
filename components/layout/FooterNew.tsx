c5c33374ef1c61366a9d126b1fd6dbd36a02a64f
Fabián Orozco <f.orozcob@uniandes.edu.co>
2025-08-06 06:39:57 -0500

    Fix footer mail

--- components/layout/FooterNew.tsx
+++ components/layout/FooterNew.tsx
@@ -111,7 +111,7 @@ export default function FooterNew() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="footer-icon icon-sm" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span className="footer-text">caballosyolamr1627</span>
+                <span className="footer-text">caballosyolamr1627@hotmail.com</span>
               </div>
             </div>
           </div>
